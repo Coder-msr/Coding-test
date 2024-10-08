49a22ed8d13e7f180f41285865dfc3e78977d274
Manendra Singh Rajput <manendrasinghrajput69@gmail.com>
2024-10-08 15:59:43 +0530

    Add files via upload

---
+++ Day - 11 Police And Theif.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+int main(void) {
+    int t,x,y;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d%d",&x,&y);
+       
+            printf("%d\n",abs (x-y));
+        
+        
+        
+    }
+	
+	return 0;
+}
